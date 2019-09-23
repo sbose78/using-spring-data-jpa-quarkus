c2fba6ecbf77fd9eec57f041966cfb42edb29d30
Pavel Macík <pavel.macik@gmail.com>
2019-09-23 20:58:48 +0200

    Add CRUD Demo UI and make the UX consistent with other demos. (#2)
    * Add CRUD DEMO front page. Add API endpoints.
    
    * Add io.quarkus/quarkus-spring-web maven dependnecy.
    
    * Fix update method, update import.sql not to interfere with new items (via generated ID).

--- src/main/java/org/acme/spring/data/jpa/FruitResource.java
+++ src/main/java/org/acme/spring/data/jpa/FruitResource.java
@@ -1,5 +1,7 @@
 package org.acme.spring.data.jpa;
 
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,11 +9,17 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import org.jboss.logmanager.Level;
+import org.jboss.logmanager.Logger;
+
 import java.util.List;
 import java.util.Optional;
 
-@Path("/fruits")
+@Path("/api/fruits")
 public class FruitResource {
+
+    private final static Logger log = Logger.getLogger(FruitResource.class.getCanonicalName());
 
     private final FruitRepository fruitRepository;
 
@@ -39,6 +47,15 @@ public class FruitResource {
         return fruitRepository.save(new Fruit(name, color));
     }
 
+    @POST
+    @Path("/")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Fruit create(JsonObject fruit) {
+        System.out.println(fruit.toString());
+        return fruitRepository.save(new Fruit(fruit.getString("name"), fruit.getString("color")));
+    }
+
     @PUT
     @Path("/id/{id}/color/{color}")
     @Produces("application/json")
@@ -53,10 +70,49 @@ public class FruitResource {
         throw new IllegalArgumentException("No Fruit with id " + id + " exists");
     }
 
+    @PUT
+    @Path("/id/{id}/name/{name}")
+    @Produces("application/json")
+    public Fruit u(@PathParam("id") Long id, @PathParam("name") String newName) {
+        Optional<Fruit> optional = fruitRepository.findById(id);
+        if (optional.isPresent()) {
+            Fruit fruit = optional.get();
+            fruit.setName(newName);
+            return fruitRepository.save(fruit);
+        }
+
+        throw new IllegalArgumentException("No Fruit with id " + id + " exists");
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Fruit update(@PathParam("id") Long id, JsonObject fruit) {
+        Optional<Fruit> optional = fruitRepository.findById(id);
+        if (optional.isPresent()) {
+            Fruit currentFruit = optional.get();
+            log.log(Level.DEBUG, String.format("Current Fruit: %s\n", currentFruit.toString()));
+            currentFruit.setName(fruit.getString("name"));
+            currentFruit.setColor(fruit.getString("color"));
+            log.log(Level.DEBUG, String.format("Updated Fruit: %s\n", currentFruit.toString()));
+            return fruitRepository.save(currentFruit);
+        }
+
+        throw new IllegalArgumentException("No Fruit with id " + id + " exists");
+    }
+
     @GET
     @Path("/color/{color}")
     @Produces("application/json")
     public List<Fruit> findByColor(@PathParam("color") String color) {
         return fruitRepository.findByColor(color);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Fruit findById(@PathParam("id") Long id) {
+        return fruitRepository.findById(id).get();
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO fruit(id, name, color) VALUES (1, 'Cherry', 'Red');
-INSERT INTO fruit(id, name, color) VALUES (2, 'Apple', 'Red');
-INSERT INTO fruit(id, name, color) VALUES (3, 'Banana', 'Yellow');
-INSERT INTO fruit(id, name, color) VALUES (4, 'Avocado', 'Green');
-INSERT INTO fruit(id, name, color) VALUES (5, 'Strawberry', 'Red');
+INSERT INTO fruit(id, name, color) VALUES (-2, 'Cherry', 'Red');
+INSERT INTO fruit(id, name, color) VALUES (-3, 'Apple', 'Red');
+INSERT INTO fruit(id, name, color) VALUES (-4, 'Banana', 'Yellow');
+INSERT INTO fruit(id, name, color) VALUES (-5, 'Avocado', 'Green');
+INSERT INTO fruit(id, name, color) VALUES (-6, 'Strawberry', 'Red');
